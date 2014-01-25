c5245ee7600e9f1bc8d0d5a85a147d8902a97b86
Alastair Lynn <arplynn@gmail.com>
2014-01-25 13:43:32 +0000

    Add checks on the ScoreBoard

--- ggj2014/src/com/funandplausible/games/ggj2014/ScoreBoard.java
+++ ggj2014/src/com/funandplausible/games/ggj2014/ScoreBoard.java
@@ -1,15 +1,30 @@
 package com.funandplausible.games.ggj2014;
 
 public class ScoreBoard {
-
-    int mScore = 0;
+    private int mScore = 0;
 
     public void losePoints(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException("cannot lose negative points");
+        }
         mScore -= i;
+        if (mScore < 0) {
+            mScore = 0;
+            throw new IllegalStateException("cannot have negative points");
+        }
     }
 
     public void winPoints(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException("cannot win negative points");
+        }
         mScore += i;
+        if (mScore < 0) {
+            throw new IllegalStateException("total score wrapped");
+        }
     }
 
+    public int getCurrentScore() {
+        return mScore;
+    }
 }
