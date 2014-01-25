52da95f601e67032cb2b0816b098838efec8f305
Alastair Lynn <arplynn@gmail.com>
2014-01-25 20:45:11 +0000

    Linear filter

--- ggj2014/src/com/funandplausible/games/ggj2014/ContentManager.java
+++ ggj2014/src/com/funandplausible/games/ggj2014/ContentManager.java
@@ -1,12 +1,14 @@
 package com.funandplausible.games.ggj2014;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class ContentManager {
 
     public Sprite loadSprite(String fileName) {
         Texture t = new Texture(Content.file(fileName));
+        t.setFilter(TextureFilter.Linear, TextureFilter.Linear);
         return new Sprite(t);
     }
 }
