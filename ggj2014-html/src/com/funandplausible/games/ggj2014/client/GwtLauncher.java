a40500fb365d88bf0fbbd196c4d785db6d892328
Alastair Lynn <arplynn@gmail.com>
2014-01-25 10:59:12 +0000

    Set prefix for GWT

--- ggj2014-html/src/com/funandplausible/games/ggj2014/client/GwtLauncher.java
+++ ggj2014-html/src/com/funandplausible/games/ggj2014/client/GwtLauncher.java
@@ -1,19 +1,22 @@
 package com.funandplausible.games.ggj2014.client;
 
-import com.funandplausible.games.ggj2014.GameRoot;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.funandplausible.games.ggj2014.Content;
+import com.funandplausible.games.ggj2014.GameRoot;
 
 public class GwtLauncher extends GwtApplication {
-	@Override
-	public GwtApplicationConfiguration getConfig () {
-		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
-		return cfg;
-	}
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(800,
+                600);
+        return cfg;
+    }
 
-	@Override
-	public ApplicationListener getApplicationListener () {
-		return new GameRoot();
-	}
+    @Override
+    public ApplicationListener getApplicationListener() {
+        Content.setPrefix("");
+        return new GameRoot();
+    }
 }
