489a3dc06f99389de0602698a6f40dc4af4db7b0
Sam Phippen <samphippen@googlemail.com>
2014-01-25 20:24:10 +0000

    Prefer marble over wood

--- ggj2014/src/com/funandplausible/games/ggj2014/GameRoot.java
+++ ggj2014/src/com/funandplausible/games/ggj2014/GameRoot.java
@@ -332,7 +332,7 @@ public class GameRoot implements ApplicationListener {
 
     private void createBackground() {
         SpriteDrawable sd = new SpriteDrawable(services().contentManager()
-                .loadSprite("background.jpg"), -1000);
+                .loadSprite("marble.png"), -1000);
         float bound = services().constantManager().getFloat("enemy_bounds");
         sd.setPosition(-bound, -bound);
         mDrawables.add(sd);
