f4e6885771aeedf562d6d584021473baa4a0c662
Alastair Lynn <arplynn@gmail.com>
2014-01-26 11:27:38 +0000

    Add a sound manager

---
+++ ggj2014/src/com/funandplausible/games/ggj2014/SoundManager.java
@@ -0,0 +1,33 @@
+package com.funandplausible.games.ggj2014;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+public class SoundManager {
+    private final Map<String, Sound> mSFX = new HashMap<String, Sound>();
+    private final Random mRNG = new Random();
+
+    public void play(String sound) {
+        Sound snd = getSound(sound);
+        float vol = 0.8f + 0.2f * mRNG.nextFloat();
+        float pitch = 0.9f + 0.24f * mRNG.nextFloat();
+        snd.play(vol, pitch, 0.0f);
+    }
+
+    private Sound getSound(String sound) {
+        Sound snd = mSFX.get(sound);
+        if (snd == null) {
+            snd = loadSound(sound);
+            mSFX.put(sound, snd);
+        }
+        return snd;
+    }
+
+    private Sound loadSound(String sound) {
+        return Gdx.audio.newSound(Content.file(sound + ".wav"));
+    }
+}
